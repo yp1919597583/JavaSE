c1f656888bea7b388371f88b878538b61135d764
一苇渡江 <1919597583@qq.com>
2022-05-16 19:03:45 +0800

    删除错传

--- 红叶博客项目/api_server/node_modules/fun2mod/index.js
+++
@@ -1,8 +0,0 @@
-// Function 2 module
-var fs = require('fs');
-var vm = require('vm');
-var run = vm.runInThisContext;
-module.exports = function(file, modname) {
-    run(fs.readFileSync(file)+'');
-    return this[modname];
-}
